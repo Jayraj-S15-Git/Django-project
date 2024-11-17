b160726c63d897db223e3aa6ff5b4eb8b72a3d85
jayraj shinde <jayrajshinde207@gmail.com>
2024-11-17 16:00:34 +0530

    Delete employeeinfo directory

--- employeeinfo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Emp
-
-class EmpForm(forms.ModelForm):
-    class Meta:
-        model=Emp
-        fields=['employeeid','employeename','Employeeaddress','mobilenumber','emailid','aadharno','department']
